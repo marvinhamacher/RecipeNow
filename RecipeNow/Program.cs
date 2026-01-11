c0f169b67e83f973a9c8eba9ac6f8318a744e982
Marvin Hamacher <Xavahus@gmail.com>
2026-01-11 20:09:44 +0100

    story-2: grobes reg

--- RecipeNow/Program.cs
+++ RecipeNow/Program.cs
@@ -37,7 +37,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
