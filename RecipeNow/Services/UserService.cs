47aba86ffdc0014fbdcec5ac900aece4c44b6ded
Marvin Hamacher <Xavahus@gmail.com>
2026-01-11 20:37:04 +0100

    story-2: Register.razor weiter

--- RecipeNow/Services/UserService.cs
+++ RecipeNow/Services/UserService.cs
@@ -1,6 +1,12 @@
 ﻿namespace RecipeNow.Services;
+using RecipeNow.Data.Contexts;
 
 public class UserService
 {
+    private readonly AuthDbContext _dbContext;
+    public UserService(AuthDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     
 }
