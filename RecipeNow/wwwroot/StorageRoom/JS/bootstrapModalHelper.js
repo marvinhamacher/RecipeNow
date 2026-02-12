c3ed589f1bb57f252f4952bfd472c2fad175fcd5
Marvin Hamacher <Xavahus@gmail.com>
2026-02-13 00:00:44 +0100

    story-5: Loeschen, Bearbeiten und Hinzufügen gehen.

--- RecipeNow/wwwroot/StorageRoom/JS/bootstrapModalHelper.js
+++ RecipeNow/wwwroot/StorageRoom/JS/bootstrapModalHelper.js
@@ -2,3 +2,9 @@
     const m = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
     m.hide();
 };
+
+window.bootstrapModalShow = (el) => {
+    if (!el) return;
+    const modal = new bootstrap.Modal(el);
+    modal.show();
+};
